457623a726a8d3e65a54530f6ff5c371c161895c
Federico Holc <federicoholc@gmail.com>
2024-06-14 17:10:21 -0300

    problema guarda un blob que no es de video

--- canvas-zoom/animacion.js
+++ canvas-zoom/animacion.js
@@ -1,6 +1,12 @@
+import { FFmpeg } from "@diffusion-studio/ffmpeg-js";
+
 const canvas = document.getElementById("miCanvas");
 const ctx = canvas.getContext("2d");
 const imageLoader = document.getElementById("imageLoader");
+
+const ffmpeg = new FFmpeg({
+  config: "gpl-extended",
+});
 
 let img = new Image();
 let scale = 1;
@@ -42,12 +48,12 @@ function animateZoom(timestamp) {
     // Dibuja la imagen escalada
     ctx.drawImage(img, x, y, width, height);
 
-    frames.push(canvas.toDataURL("image/jpg")); // Guarda el cuadro actual
+    frames.push(canvas.toDataURL("image/png")); // Guarda el cuadro actual
 
     // Aumenta la escala
     scale *= scaleFactor;
 
-    if (elapsed < 5000) {
+    if (elapsed < 2000) {
       // 5 segundos
       requestAnimationFrame(step);
     } else {
@@ -59,11 +65,43 @@ function animateZoom(timestamp) {
   requestAnimationFrame(step);
 }
 
+document.querySelector("#download").addEventListener("click", downloadFrames);
+
 function downloadFrames() {
-  frames.forEach((frame, index) => {
+  ffmpeg.whenReady(async () => {
+    console.log("inicio de ffmpeg");
+    await ffmpeg.writeFile("0.png", frames[0]);
+    await ffmpeg.writeFile("1.png", frames[1]);
+    await ffmpeg.writeFile("2.png", frames[2]);
+
+    let rta = await ffmpeg
+      .input({
+        sequence: ["0.png", "1.png", "2.png"],
+        framerate: 1,
+        duration: 3,
+      })
+      .ouput({
+        format: "mp4",
+        video: {
+          codec: "libx264",
+
+          framerate: 1,
+        },
+      })
+      .export();
+
+    console.log("rta:", rta);
+    console.log("fin de ffmpeg");
+    const blob = new Blob(rta, { type: "video/mp4" });
+    console.log(blob);
+    const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
-    a.href = frame;
-    a.download = `frame_${index}.jpg`;
+    a.href = url;
+    a.download = "video.mp4";
+
+    document.body.appendChild(a);
     a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
   });
 }
